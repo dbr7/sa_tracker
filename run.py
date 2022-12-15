a2d5254e79dd408ea704fa2c35b067f57ee5e34c
jinhankim <saverlip@gmail.com>
2022-12-15 05:43:41 +0000

    Update fetching script

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
-import json, os, html
-from scholarly import scholarly, ProxyGenerator
+import json, os, html, sqlite3, argparse
 
-is_save = True
-out_rank_path = "./rank.csv"
-icse_2019_data_path = "./data/icse_2019.json"
+from scholarly import scholarly, ProxyGenerator
+from datetime import datetime, timezone, timedelta
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--db-path", default="/home/sa_tracker/cite.db", type=str)
+parser.add_argument("--paper-list", default="/home/sa_tracker/data/icse_2019.json", type=str)
+parser.add_argument("--table-name", default="icse2019", type=str)
+args = parser.parse_args()
+
+DB_PATH = args.db_path
+PAPER_LIST_PATH = args.paper_list
+TABLE_NAME = args.table_name
+
+timezone_korea = timezone(timedelta(hours=9))
+
 
 def load_papers_from_json(json_path):
     with open(json_path, "r") as f:
@@ -18,43 +29,60 @@ def load_papers_from_json(json_path):
     return r
 
 
-def fetch(target_paper_title, is_save=False):    
+def fetch(target_paper_title):    
     search_query = scholarly.search_pubs(target_paper_title)
     paper = scholarly.fill(next(search_query))
-
+    
     num_citation = paper["num_citations"]
     title = paper["bib"]["title"]  
+    bib_id = paper["bib"]["bib_id"]
 
     if "booktitle" in paper["bib"]:
+        exact = 1
         venue = paper["bib"]["booktitle"]
     else:
         print("This has a prob")
+        exact = 0
         venue = paper["bib"]["venue"]
 
-       
-    print(title)
-    print(venue)
-    print(num_citation)
-
-    if is_save:
-        with open(out_rank_path, "a") as f:
-            f.write(f"{title},{num_citation}\n")
+    return bib_id, title, num_citation, exact
 
 
+if __name__ == "__main__":    
+    # Load papers
+    papers = load_papers_from_json(PAPER_LIST_PATH)
+    
+    # DB lookup
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()    
 
-if __name__ == "__main__":
-    if is_save and os.path.exists(out_rank_path):
-        os.remove(out_rank_path)
+    c.execute(f"SELECT * FROM {TABLE_NAME}")
+    db_papers = c.fetchall()    
 
+    # Set up a proxy    
     pg = ProxyGenerator()
     success = pg.FreeProxies()
     scholarly.use_proxy(pg)
 
-    icse_2019_papers = load_papers_from_json(icse_2019_data_path)
+    # c.execute(f"DELETE FROM {TABLE_NAME}")    
+    current_time = datetime.now(timezone_korea)
 
-    for _p in icse_2019_papers:    
-        _p = html.unescape(_p)
-        print("="*30)
-        print(f"Fetching '{_p}'")
-        fetch(_p, is_save=is_save)
-        # break
+    for json_p in papers:
+        json_p = html.unescape(json_p)
+        is_fetched = False
+        for db_p in db_papers:
+            _id, _title, _num_citation, _exact, _current_time, _jsontitle = db_p
+            if json_p == _jsontitle:                
+                is_fetched = True
+                break
+        
+        if not is_fetched or (current_time - datetime.fromisoformat(_current_time)).total_seconds() > 60 * 60 * 12:
+            print(f"Fetching '{json_p}'")
+            p_id, p_title, p_num_citation, p_exact = fetch(json_p)
+            
+            print(p_id, p_title, json_p, p_num_citation, p_exact, current_time)
+            c.execute(
+                f"INSERT INTO {TABLE_NAME} (id, title, jsontitle, citation, exact, dt) VALUES (?, ?, ?, ?, ?, ?)",
+                (p_id, p_title, json_p, p_num_citation, p_exact, current_time)
+            )
+            conn.commit()
